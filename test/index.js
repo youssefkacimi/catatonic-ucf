e286e9244224e1a1f3e1db5cf4368a37049031df
youssefkacimi <kacimi.ucf@gmail.com>
2015-12-01 18:21:02 +0100

    import chai

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
-import assert from 'assert';
+import { should } from 'chai';
 import catatonicCircle from '../lib';
+
+/*import assert from 'assert';
+import catatonicCircle from '../lib';*/
 
 /*describe('y', function () {
   it('should have unit test!', function () {
@@ -7,8 +10,16 @@ import catatonicCircle from '../lib';
   });
 });*/
 
-describe('catatonic-ucf', function () {
+/*describe('catatonic-ucf', function () {
   it('should have a version number!', function () {
     assert(typeof catatonicCircle.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+  });
+});*/
+
+should(); // Modifies objects prototype to include the 'should' property
+
+describe('catatonic-circle', function () {
+  it('should have a version number!', function () {
+    catatonicCircle.should.have.property('VERSION');
   });
 });
